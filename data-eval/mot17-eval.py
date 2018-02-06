a7fe78d59eb025b33928da5efbd70679ad0585be
Reluigi <luigi@isecondo.eu>
2018-02-06 19:12:07 +0100

    This shit is shit!

--- data-eval/mot17-eval.py
+++ data-eval/mot17-eval.py
@@ -9,8 +9,11 @@ Python dictionary / json format:
 """
 import glob
 import json
+import numpy as np
+import pandas as pd
+
 # Change this with the path of labels folder
-path = "/Users/luigi/Desktop/label_2/"
+gt_path = "/Users/luigi/Desktop/MOD17/MOT17Det/train/MOT17-02/gt/gt.txt"
 
 def file_len(fo):
     """ Number of lines per file
@@ -27,51 +30,12 @@ if __name__ == "__main__":
     The main purpose of this script is to understand how many pictures contain pedestrians and compute an average of pedestrians per picture.
 
     """
+    gt = pd.read_csv(gt_path, header=None ).values
+    gt_sorted = np.sort(gt, axis=0)
 
     data = dict()
     pedestrians = 0
-    ped_pictures = 0
-    tot_pictures = 0
-
-    for current_file in glob.glob(path + '*.txt'):
-
-        tot_pictures += 1
-        # count number of lines in the file
-        lines = file_len(current_file)
-        fo = open(current_file, "r")
-        bboxes = []
-        for x in range(lines):
-            """Check pedestrian values
-
-            I split the line every time I find a space and then analyze the values
-            """
-            line = fo.readline()
-            words = line.split(" ")
-            if words[0] == "Pedestrian":
-                pedestrians += 1
-                coordinates = []
-                coordinates.append(float(words[4]))
-                coordinates.append(float(words[5]))
-                coordinates.append(float(words[6]))
-                coordinates.append(float(words[7]))
-                bboxes.append(coordinates)
-
-        # Close opened file
-        fo.close()
-
-        # Add things to the dictionary
-        path_words = current_file.split("/")
-        picture_code = path_words[-1].split(".")
-        data[picture_code[0] + '.png'] = bboxes
-        if bboxes:
-            ped_pictures += 1
-
-    # write results in json file
-    json.dump(data, open("/Users/luigi/Git/sofar-obstacle-detection/" + '/kitti-label.json', 'w'))
-
-    print("\nTotal number of pictures: ", tot_pictures)
-    print("Total number of pedestrians: ", pedestrians)
-    print("Total number of pictures with pedestrians: ", ped_pictures)
-    print("Total number of pictures without pedestrians: ", tot_pictures - ped_pictures)
-    print("Average of pedestrians per picture with pedestrians: ", pedestrians / ped_pictures)
-    print("Average of pedestrians per picture in general: ", pedestrians / tot_pictures)
+    tot_pictures = 600
+    for row in gt_sorted:
+        print(row)
+        break
